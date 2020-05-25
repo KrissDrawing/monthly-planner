c3db0873574a3ed49263aafaf23631747bfdb74e
krissdrawing <krx95@o2.pl>
2020-05-25 13:04:29 +0200

    Updates

---
+++ precache-manifest.cb008a9e955f51f6b5b20a9718602126.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059c03bdb11a902fa8072935dfaeb66d",
+    "url": "/monthly-planner/index.html"
+  },
+  {
+    "revision": "fff41bda3e6e78523e1d",
+    "url": "/monthly-planner/static/css/2.2e898540.chunk.css"
+  },
+  {
+    "revision": "bad76e40caa5ab2c38ab",
+    "url": "/monthly-planner/static/css/main.d99bc1a8.chunk.css"
+  },
+  {
+    "revision": "fff41bda3e6e78523e1d",
+    "url": "/monthly-planner/static/js/2.c05c67e8.chunk.js"
+  },
+  {
+    "revision": "fbbfe9ed4dc77af342a4a500830ec9a4",
+    "url": "/monthly-planner/static/js/2.c05c67e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad76e40caa5ab2c38ab",
+    "url": "/monthly-planner/static/js/main.8f30216f.chunk.js"
+  },
+  {
+    "revision": "3e6b268013ee74389a0e",
+    "url": "/monthly-planner/static/js/runtime-main.af0a392b.js"
+  }
+]);
